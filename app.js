b5a5f5c9e569ff19e383ca9796277b87e9dfceb8
Eduardo Vásquez <30580824+EduardoVasquez9@users.noreply.github.com>
2020-03-26 14:29:15 -0600

    Queries ready

--- app.js
+++ app.js
@@ -44,10 +44,10 @@ app.get('/transaction/:id', (req, res)=>{
 
 app.post('/transaction', (req, response) => {
     client.query("INSERT INTO" + 
-      "salesforce.transaction__c()" +
-      "VALUES ($1, $2, $3, $4)", 
+      "salesforce.transaction__c(Name, OrderID__c, ReasonCodeDesc__c, ReasonCode__c, ResponseCode__c, transaction_appid__c)" +
+      "VALUES ($1, $2, $3, $4, $5, $6)", 
       
-      [req.body.Cantidad__c, ], 
+      [req.body.Name, req.body.OrderID, req.body.ReasonCodeDesc, req.body.ReasonCode, req.body.ResponseCode, req.body._id], 
       
       (err, data) => {
         if (err) {
