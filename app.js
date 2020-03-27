76e9022526d772ae5ff1a5e0aba973d9c293cd93
Alexy Cruz <alexyc@unitec.edu>
2020-03-27 00:36:20 -0600

    Redirect changed

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.post('/transaction', (req, response) => {
           console.log(err);
           response.status(400).send(err);
         } else {
-          response.redirect('https://delcampo.force.com/s/enrollment?id=' + data.rows[0].ReasonCodeDesc__c);
+          response.redirect('https://delcampo.force.com/s/quotes?id=' + data.rows[0].ReasonCodeDesc__c);
         }
     });
   });
