55df22fc7d82dc35b4b1f529b908708225dea3dc
Alexy Cruz <alexyc@unitec.edu>
2020-04-08 16:33:24 -0600

    replace all

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.post('/transaction', (req, response) => {
           console.log(err);
           response.status(400).send(err);
         } else {
-          let reasonDesc = data.rows[0].reasoncodedesc__c.toString().replace(/+/g, ' ');
+          let reasonDesc = data.rows[0].reasoncodedesc__c.toString().replace(/\+/g, ' ');
           console.log(reasonDesc);
           response.redirect('https://delcampo.force.com/s/bills?id=' + reasonDesc + "&id2=" + data.rows[0].reasoncode__c);
         }
